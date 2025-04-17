94e459fade5cad17e082704379129083ff61b6d7
Gaston Paez <paez.gastonm@gmail.com>
2025-04-17 15:59:51 -0300

    feat: deploy render

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
-
+import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT, default=5000"))
